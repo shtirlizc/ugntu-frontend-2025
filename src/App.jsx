848de49b090a3eb8c08a3b342bebda13df38271f
Marat Sarvarov <shtirlizc@gmail.com>
2025-11-24 01:09:19 +0500

    tasks crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-
-import './App.css'
 import React, {useEffect, useState} from 'react';
+
+
+import {  Button, Flex, Input } from 'antd';
+
 import { supabase } from './supabaseClient.js';
+import {Task} from "./task.jsx";
 
 function App() {
     const [tasks, setTasks] = useState([]);
@@ -18,17 +21,55 @@ function App() {
         getTodos().then();
     }, []);
 
+    const [newTask, setNewTask] = useState('');
+    const handleChangeNewTask = (e) => {
+        setNewTask(e.target.value);
+    }
+
+    const addNewTask = async () => {
+        const response = await supabase.from('tasks').insert({ name: newTask.trim(), done: false }).select();
+
+        if (response.error === null) {
+            const insertedTask = response.data.at(0);
+            setTasks([...tasks, insertedTask]);
+            setNewTask('');
+        } else {
+            console.error('Error adding task', response.error);
+        }
+    }
+
+    const onUpdateTask = (id, name, done) => {
+        setTasks(tasks.map(task => {
+            if (task.id === id) {
+               return {
+                   ...task,
+                   name,
+                   done,
+               }
+            }
+
+            return task;
+        }));
+    }
+    const onDeleteTask = (id) => {
+        setTasks(tasks.filter(task => task.id !== id));
+    }
+
+    const sortedTasks = tasks.sort((a, b) => {
+        return a.id - b.id;
+    })
+
   return (
-    <div>{
-        tasks.map(task => {
+    <Flex vertical gap={16} style={{ maxWidth: 400, padding: 20 }}>
+        {sortedTasks.map(task => {
             return (
-                <div key={task.id}>
-                    <span>{task.name}</span>{' '}
-                    <input type="checkbox" checked={task.done} />
-                </div>
+                <Task key={task.id} task={task} onUpdateTask={onUpdateTask} onDeleteTask={onDeleteTask} />
             )
         })
-    }</div>
+    }
+        <Input placeholder="New task" value={newTask} onChange={handleChangeNewTask} />
+        <Button type="primary" disabled={newTask.trim() === ''} onClick={addNewTask}>Add task</Button>
+    </Flex>
   )
 }
 
---
+++ src/task.jsx
@@ -0,0 +1,71 @@
+import {Checkbox, Button, Flex, Input} from "antd";
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import React from "react";
+
+import { supabase } from './supabaseClient.js';
+
+export const Task = (props) => {
+    const { task } = props;
+
+    const [isEditMode, setIsEditMode] = React.useState(false);
+    const [taskName, setTaskName] = React.useState(task.name);
+
+    const handleEditTask = () => {
+        setIsEditMode(true);
+    }
+
+    const handleChangeTaskName = (e) => {
+        setTaskName(e.target.value);
+    }
+
+    const handleRenameTask = async () => {
+        setIsEditMode(false);
+
+        const response = await supabase.from('tasks').update({ name: taskName.trim() }).eq('id', task.id);
+
+        if (response.error === null) {
+            props.onUpdateTask(task.id, taskName.trim(), task.done);
+        } else {
+            console.error('Error editing task', response.error);
+        }
+    }
+
+    const handleDoneTask = async (e) => {
+        const response = await supabase.from('tasks').update({ done: e.target.checked }).eq('id', task.id);
+
+        console.log('response', response);
+
+        if (response.error === null) {
+            props.onUpdateTask(task.id, task.name, e.target.checked);
+        } else {
+            console.error('Error editing task', response.error);
+        }
+    }
+
+    const handleDeleteTask = async () => {
+        const response = await supabase.from('tasks').delete().eq('id', task.id);
+
+        if (response.error === null) {
+            props.onDeleteTask(task.id);
+        } else {
+            console.error('Error deleting task', response.error);
+        }
+    }
+
+    if (isEditMode) {
+        return <Flex gap={12} align='center'>
+            <Input placeholder="Task name" value={taskName} onChange={handleChangeTaskName} />
+            <Button type="primary" disabled={taskName.trim() === ''} onClick={handleRenameTask}>Rename</Button>
+        </Flex>
+    }
+
+    return (
+        <Checkbox checked={task.done} onChange={handleDoneTask}>
+            <Flex gap={12} align='center'>
+                <Button type="primary" shape="circle" icon={<EditOutlined />} onClick={handleEditTask} disabled={task.done} />
+                <Button type="primary" shape="circle" icon={<DeleteOutlined />} danger onClick={handleDeleteTask} />
+                {task.name}
+            </Flex>
+        </Checkbox>
+    )
+}
